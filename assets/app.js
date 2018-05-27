e0bcd0841b21f68dcb219b2af1cff0120fbe37d2
Cecily Grossmann <grssmnn2@gmail.com>
2018-05-27 10:05:22 -0500

    added winner images

--- assets/app.js
+++ assets/app.js
@@ -12,21 +12,27 @@ document.addEventListener("keyup", function () {
     if (userChoice === "r" || userChoice ==="p" || userChoice ==="s") {
         if (userChoice === "r" && computerGuess === "p") {
             losses++;
+            document.getElementById("winner").innerHTML = "<img src='assets/images/paper.jpg'/>"
             document.getElementById("announce").innerHTML = "You lost!";
         } else if (userChoice === "p" && computerGuess === "r") {
             wins++;
+            document.getElementById("winner").innerHTML = "<img src='assets/images/paper.jpg'/>"
             document.getElementById("announce").innerHTML = "You win!!!!";
         } else if (userChoice === "s" && computerGuess === "p") {
             wins++;
+            document.getElementById("winner").innerHTML = "<img src='assets/images/scissors.jpg'/>"
             document.getElementById("announce").innerHTML = "You win!!!!";
         } else if (userChoice === "p" && computerGuess === "s") {
             losses++;
+            document.getElementById("winner").innerHTML = "<img src='assets/images/scissors.jpg'/>"
             document.getElementById("announce").innerHTML = "You lost!";
         } else if (userChoice === "r" && computerGuess === "s") {
             wins++;
+            document.getElementById("winner").innerHTML = "<img src='assets/images/rock.jpg'/>"
             document.getElementById("announce").innerHTML = "You win!!!!";
         } else if (userChoice === "s" && computerGuess === "r") {
             losses++;
+            document.getElementById("winner").innerHTML = "<img src='assets/images/rock.jpg'/>"
             document.getElementById("announce").innerHTML = "You lost!";
         } else if (userChoice === computerGuess) {
             ties++;
